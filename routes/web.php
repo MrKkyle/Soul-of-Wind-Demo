f2c33b6f9d19852ac6b085f777af41dbc1ef2151
MrKkyle <Kyle.apocrypha@gmail.com>
2023-11-16 18:55:00 +0200

    updated

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* The main .blade file should be configured below (most likely index if you using react */
+/* Main Routes & Controllers */
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registerController;
+use App\Http\Controllers\sessionController;
+use App\Http\Controllers\workerController;
+
+/* The main .blade file should be configured below (most likely index) if you using react */
 Route::get('/', function () 
 {
     return view('index');
@@ -22,12 +28,6 @@ Route::get('/', function ()
 /* React Routes */
 Route::view('/{path?}', 'index')
     ->where('path', '.*');
-
-/* Main Routes & Controllers */
-use App\Http\Controllers\loginController;
-use App\Http\Controllers\registerController;
-use App\Http\Controllers\sessionController;
-use App\Http\Controllers\workerController;
 
 Route::post('/session_variables', [sessionController::class, 'session_variables']);
 Route::post('/register', [registerController::class, 'register']);
