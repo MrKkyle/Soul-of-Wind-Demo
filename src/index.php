bfa67ef918c919f0b5d1ebc2d643dcc535b6c389
Keenan-Faure <keenan@stock2shop.com>
2024-03-05 14:50:02 +0200

    updated docker compose file

--- src/index.php
+++ src/index.php
@@ -1,19 +1,17 @@
 <?php
-/*
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
@@ -23,8 +21,4 @@ $response = $kernel->handle(
 
 $kernel->terminate($request, $response);
 
-*/
-
-phpinfo();
-
-
+// phpinfo();
